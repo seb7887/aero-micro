e69b534acd06745781692744f2a7669bf3842af8
Sebastian <segurasebastian7887@gmail.com>
2019-04-24 00:21:35 -0300

    Cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('micro-cors')();
 const { router, get } = require('microrouter');
 
 const me = require('./me');
@@ -5,9 +6,11 @@ const points = require('./points');
 const products = require('./products');
 const notfound = require('./notfound');
 
-module.exports = router(
-  get('/user/me', me),
-  get('/user/points', points),
-  get('/products', products),
-  get('/*', notfound)
+module.exports = cors(
+  router(
+    get('/user/me', me),
+    get('/user/points', points),
+    get('/products', products),
+    get('/*', notfound)
+  )
 );
